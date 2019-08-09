d632b71330d8a9f1a4fbd77d681fbb5f652e6b7e
Jason Aviles <sandman2k18@outlook.com>
2019-08-09 16:39:45 -0400

    forgotto push

--- index.js
+++ index.js
@@ -5,50 +5,37 @@ const cors = require("cors");
 server.use(bodyParser.json());
 server.use(cors());
 
-const port =  5000;
+const port = 8000;
 
+let data = [
+  {
+    id: 1,
+    name: "Frodo Baggins",
+    chores: [
+      {
+        id: 1,
+        description: "take the ring to Mordor",
+        notes: "make your way to Mount Doom",
+        assignedTo: 1, // the id of Frodo,
+        completed: true
+      },
+      {
+        id: 2,
+        description: "destroy the ring",
+        notes: "cast the ring into the fire inside Mount Doom",
+        assignedTo: 1,
+        completed: false
+      }
+    ]
+  }
+];
 
+server.get("/chores", (req, res) => {
+  res.json(data);
+});
 
-let data =[{
-  id: 1,
-  name: 'Frodo Baggins',
-  chores: [
-    {
-      id: 1,
-      description: 'take the ring to Mordor',
-      notes: 'make your way to Mount Doom',
-      assignedTo: 1, // the id of Frodo,
-      completed: true
-    },
-    {
-      id: 2,
-      description: 'destroy the ring',
-      notes: 'cast the ring into the fire inside Mount Doom',
-      assignedTo: 1,
-      completed: false
-    },
-  ]
-},
-
-]
-
-console.log()
-
-
-
-server.get('/', (req,res)=>{
-
-  
-res.json(data)
-
-
-
-})
-
-
-
-server.get('/people/:id', (req,res)=>{
-  const chores = data.map(x =>x.chores)
+server.get("/chores/:id", (req, res) => {
+  const chores = data.map(x => x.chores);
   const { id } = req.params;
 
   const findById = people => {
@@ -56,81 +43,69 @@ server.get('/people/:id', (req,res)=>{
   };
   const gettingPerson = data.find(findById);
 
-  if(!gettingPerson){
-    res.status(400).json({message:'no id'})
-  }else{
-
+  if (!gettingPerson) {
+    res.status(400).json({ message: "no id" });
+  } else {
     if (chores) gettingPerson.chores = chores;
-    res.json(data)
+    res.json(data);
   }
+});
 
-})
+server.get("/chores/completed", (req, res) => {
+  const completed = req.query.completed;
+  const Datapeople = data.map(x => x.chores);
 
+  if (completed) {
+    const filter = completed === "true" ? true : false;
+    const choresMap = Datapeople.filter(x => x.completed === filter);
+    res.status(200).json(choresMap);
+  } else {
+    res.status(200).json(Datapeople);
+  }
+});
+let peopleId = 2;
 
-server.get("/completed", (req,res)=>{
-  const Datapeople = data.map(x =>x.chores )
- const choresMap =  Datapeople.map(x => x )
- console.log(choresMap)
- 
-    res.json(choresMap)
-  
+server.post("/", (req, res) => {
+  const { name } = req.body;
+  const Newpeople = { name, id: peopleId };
+  const findByName = data => {
+    return data.name === name;
+  };
+  if (data.find(findByName)) {
+    res.status(400).json({ message: "person exsit" });
+  }
+  data.push(Newpeople);
+  peopleId++;
+  res.json(data);
+});
 
-})
-let peopleId = 1;
-
-server.post('/', (req,res)=>{
-
-  const {name} = req.body
-const  Newpeople ={name ,id:peopleId}
-const findByName = data => {
-  return data.name === name;
-};
-if(data.find(findByName)){
-res.status(400).json({message:'person exsit'})
-}
-data.push(Newpeople);
-peopleId++;
-res.json(data);
-})
-
-
-
-server.put("/", (req,res)=>{
+server.put("/", (req, res) => {
   const { id } = req.params;
   const findById = data => {
     return data.id == id;
   };
 
-const foundPeople = data.find(findById)
-if(!foundPeople){
-  res.status(400).json({message:'no id'})
-}
-else{
-  if (name) foundPeople.name = name
+  const foundPeople = data.find(findById);
+  if (!foundPeople) {
+    res.status(400).json({ message: "no id" });
+  } else {
+    if (name) foundPeople.name = name;
 
-res.json(data)
-}
-})
+    res.json(data);
+  }
+});
 
-
-
-server.delete('/',(req,res)=>{
+server.delete("/", (req, res) => {
   const { id } = req.params;
   const foundData = data.find(people => people.id == id);
 
   if (foundData) {
-    const SmurfRemoved = { ...foundData };
+    const peopleRemoved = { ...foundData };
     data = data.filter(people => people.id != id);
     res.status(200).json(data);
   } else {
-   res.status(401).json("No smurf by that ID exists in the smurf DB", res);
+    res.status(401).json("No  DB", res);
   }
-})
-
-
-
-
-
-
+});
 
 server.listen(port, () => console.log(`connected to port ${port}`));
